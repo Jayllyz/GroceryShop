49e7378d0f9f4f593dfb6174183273bfb8e689ea
Jayllyz <antonydavid945@gmail.com>
2019-12-19 22:38:02 +0100

    first day

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world ! \n");
+    int caractereActuel = 0;
+    FILE* fichier = NULL;
+    fichier = fopen("vitrine.txt", "r");
+    do
+    {
+        caractereActuel = fgetc(fichier); // On lit le caractère
+        printf("%c", caractereActuel);
+    } while ( caractereActuel != EOF);
+    
+    fclose(fichier);
+
     return 0;
 }
---
+++ modules/payement.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float payement(float prix)
+{
+    float sommeClient;
+    float sommeRendre;
+    scanf("%f", &sommeClient);
+    if ( (sommeClient - prix) == 0) //Le payement est bon rien à rendre
+    {
+        return 0; 
+    }
+    if( (sommeClient - prix) <  0) //Le mec à pas suffisament d'argent 
+    {
+        printf("Payement refuse, somme inssufisante");
+        exit(-1);
+    }
+    if((sommeClient - prix) > 0) 
+    {
+        sommeRendre = sommeClient - prix; //On calcul la monnaie à rendre
+        return sommeRendre;
+    }
+}
