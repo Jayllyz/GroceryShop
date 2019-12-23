4a622842cc45bec917143dce4f555f46da8befb1
Jayllyz <antonydavid945@gmail.com>
2019-12-23 11:20:31 +0100

    anto
    update main.c / vitrine

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void cursorPosition(int YPos, int XPos)
+ {
+    printf("\033[%d;%dH",YPos+1,XPos+1);
+}
 
 
 int main()
 {
+	system("clear");
+
+
+	int emojiF[3][5];
+	int prixF[15];
+	
+	int emojiL[3][5];
+	int prixL[15];
+
+	int emojiV[3][5];
+	int prixV[15];
+
+
     int caractereActuel = 0;
     FILE* fichier = NULL;
     fichier = fopen("vitrine.txt", "r");
@@ -13,6 +32,10 @@ int main()
         printf("%c", caractereActuel);
     } while ( caractereActuel != EOF);
     
+    cursorPosition(20,100);
+    printf("salut");
+
+
     fclose(fichier);
 
     return 0;
