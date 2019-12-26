0bee29ac9dfa112dc7da368b888e7d97b890131d
Jayllyz <antonydavid945@gmail.com>
2019-12-26 16:33:47 +0100

    initTab prix/Code

--- main.c
+++ main.c
@@ -14,13 +14,64 @@ int main()
 
 
 	int emojiF[3][5];
-	int prixF[15];
-	
+	float *prixF[15];
+    char codeF[15][4] = 
+    { 
+        "F00",
+        "F01",
+        "F02",
+        "F03",
+        "F04",
+        "F05",
+        "F06",
+        "F07",
+        "F08",
+        "F10",
+        "F11",
+        "F12",
+        "F13",
+        "F14",
+    };
+
 	int emojiL[3][5];
-	int prixL[15];
+	float prixL[15];
+    char codeL[15][4] = 
+    { 
+        "L00",
+        "L01",
+        "L02",
+        "L03",
+        "L04",
+        "L05",
+        "L06",
+        "L07",
+        "L08",
+        "L10",
+        "L11",
+        "L12",
+        "L13",
+        "L14",
+    };
 
 	int emojiV[3][5];
-	int prixV[15];
+	float prixV[15];
+    char codeV[15][4] = 
+    { 
+        "V00",
+        "V01",
+        "V02",
+        "V03",
+        "V04",
+        "V05",
+        "V06",
+        "V07",
+        "V08",
+        "V10",
+        "V11",
+        "V12",
+        "V13",
+        "V14",
+    };
 
 
     int caractereActuel = 0;
---
+++ modules/initTab.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void initPrixF(float tab[])
+{
+    tab[0] = 3.4;
+    tab[1] = 4.1;
+    tab[2] = 2.9;
+    tab[3] = 2.2;
+    tab[4] = 2;
+    tab[5] = 3.1;
+    tab[6] = 1;
+    tab[7] = 2.5;
+    tab[8] = 3.8;
+    tab[9] = 2.5;
+    tab[10] = 2;
+    tab[11] = 1.6;
+    tab[12] = 1.8;
+    tab[13] = 3.7;
+    tab[14] = 1.5;
+}
+
+
+void initPrixL(float tab[])
+{
+    tab[0] = 2.4;
+    tab[1] = 2.1;
+    tab[2] = 1.9;
+    tab[3] = 2.8;
+    tab[4] = 1.7;
+    tab[5] = 2.1;
+    tab[6] = 2.9;
+    tab[7] = 2.9;
+    tab[8] = 1.9;
+    tab[9] = 3.1;
+    tab[10] = 2.1;
+    tab[11] = 2.5;
+    tab[12] = 2.8;
+    tab[13] = 2.3;
+    tab[14] = 2.7;
+}
+
+void initPrixV(float tab[])
+{
+    tab[0] = 3.2;
+    tab[1] = 3.9;
+    tab[2] = 4.3;
+    tab[3] = 3.8;
+    tab[4] = 4.9;
+    tab[5] = 5;
+    tab[6] = 5.1;
+    tab[7] = 2.3;
+    tab[8] = 3.8;
+    tab[9] = 4;
+    tab[10] = 5.4;
+    tab[11] = 3.1;
+    tab[12] = 4.3;
+    tab[13] = 4.8;
+    tab[14] = 5.1;
+}
+
+
+
